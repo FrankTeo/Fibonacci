c5976e1947d6cb797a47e31488a8880325efff90
Frank Teo <34079620+FrankTeo@users.noreply.github.com>
2019-03-02 08:28:51 +1300

    add Test
    add Test

--- fibonacci.c
+++ fibonacci.c
@@ -1,11 +1,18 @@
 
-#include<stdio.h>
+#include <assert.h>
+#include <stdio.h>
 int fib(int n)
 {
     if (n <= 1)
         return n;
     else
         return fib(n-1) + fib(n-2);
+}
+
+void Test_fib()
+{
+    assert(fib(11) == 89);
+    assert(fib(17) == 1597);
 }
  
 int main()
@@ -20,6 +27,8 @@ int main()
         if (i != 0 && i%5 == 0)
             printf("\n");
     }
-    return 0;
 
+    printf("\n");
+    Test_fib();
+    return 0;
 }
